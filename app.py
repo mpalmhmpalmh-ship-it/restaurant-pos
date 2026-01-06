ab621ecb010b0629b7db5690b5f0f092025c3421
mpalmhmpalmh-ship-it <mpalmhmpalmh@gmail.com>
2026-01-06 14:32:09 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def pos():
     return "POS PAGE"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
